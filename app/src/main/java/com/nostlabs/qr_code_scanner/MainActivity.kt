531db776e01c097f74715d196c9aab6e0d95c618
Tim Acosta <timacosta06@gmail.com>
2021-07-09 17:07:08 +0100

    added camera permission

--- app/src/main/java/com/nostlabs/qr_code_scanner/MainActivity.kt
+++ app/src/main/java/com/nostlabs/qr_code_scanner/MainActivity.kt
@@ -1,25 +1,48 @@
 package com.nostlabs.qr_code_scanner
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.nostlabs.qr_code_scanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val permissions = arrayOf("android.permission.CAMERA", "android.permission.INTERNET")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.btnQRScanner.setOnClickListener {
-            Toast.makeText(this,"Pressed button", Toast.LENGTH_SHORT).show()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(checkCameraPermission()) startActivity(intent)
+            requestCameraPermission()
         }
-
-
-
-
     }
+
+    fun checkCameraPermission(): Boolean {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+                return false;
+        }
+        return true;
+    }
+
+    fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, permissions,1)
+    }
+
+
+
 }
---
+++ app/src/main/java/com/nostlabs/qr_code_scanner/QRCodeAnalyzer.kt
@@ -0,0 +1,14 @@
+package com.nostlabs.qr_code_scanner
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+
+class QRCodeAnalyzer {
+
+
+
+
+
+}
