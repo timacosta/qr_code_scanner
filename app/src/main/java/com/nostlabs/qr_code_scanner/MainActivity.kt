11094a782fe3d1a3fa70575ea6c4edb40e0bbe86
Tim Acosta <timacosta06@gmail.com>
2021-07-09 15:48:54 +0100

    added first view

--- app/src/main/java/com/nostlabs/qr_code_scanner/MainActivity.kt
+++ app/src/main/java/com/nostlabs/qr_code_scanner/MainActivity.kt
@@ -2,10 +2,24 @@ package com.nostlabs.qr_code_scanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.nostlabs.qr_code_scanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnQRScanner.setOnClickListener {
+            Toast.makeText(this,"Pressed button", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 }
